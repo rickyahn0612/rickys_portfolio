8ea2c61853d10450c6a895b406cab410549d8c89
rickyahn0612 <rickyahn0612@yahoo.com>
2018-03-06 08:00:49 -0800

    adding in port view

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,24 @@
 // This is where it all goes :)
+
+$( document ).ready(function() {
+  $('.portfolio-image-container img').click(function(){
+    $('.left-container').fadeOut(function(){
+      $('html, body').animate({ scrollTop: 0 }, 1500);
+    });
+    $('.right-container').animate({'left': 0}, 1500, function(){
+      $('.port-view-container').fadeIn(1000, function(){
+        $('.close-button').fadeIn();
+      });
+    });
+  });
+
+  $('.close-button').click(function(){
+    $('.port-view-container').fadeOut(function() {
+      $('.right-container').animate({'left': '50%'}, 1200, function(){
+        $('.close-button').animate({top: -1000}, 1000);
+        $('.left-container').fadeIn()
+      });
+    })
+
+  })
+});
