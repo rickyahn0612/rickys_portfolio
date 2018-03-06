ca3289661c87087fc3ad160f157caaef39015096
rickyahn0612 <rickyahn0612@yahoo.com>
2018-03-06 11:24:23 -0800

    mobile view for port page

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,24 +1,23 @@
 // This is where it all goes :)
 
 $( document ).ready(function() {
-  $('.portfolio-image-container img').click(function(){
+  $('.portfolio-image-container img, .port-button').click(function(){
     $('.left-container').fadeOut(function(){
       $('html, body').animate({ scrollTop: 0 }, 1500);
     });
-    $('.right-container').animate({'left': 0}, 1500, function(){
+    $('.right-container').addClass('mobile-view').animate({'left': 0}, 1500, function(){
       $('.port-view-container').fadeIn(1000, function(){
-        $('.close-button').animate({'opacity' : 1, top: 0});
+        $('.close-button').animate({'opacity' : 1, top: 0}).addClass('close-button-mobile');
       });
     });
   });
 
   $('.close-button').click(function(){
-    $('.port-view-container').fadeOut(function() {
-      $('.right-container').animate({'left': '50%'}, 1200, function(){
-        $('.close-button').animate({top: -1000}, 1000);
+    $('.close-button').animate({top: -1000}, 1000).removeClass('close-button-mobile', function() {
+      $('.port-view-container').fadeOut(function() {
+      $('.right-container').removeClass('mobile-view').animate({'left': '50%'}, 1200)
         $('.left-container').fadeIn()
       });
     })
-
   })
 });
