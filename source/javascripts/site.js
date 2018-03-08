9f09925979f8ce3c8724730c43af60a8a853f365
rickyahn0612 <rickyahn0612@yahoo.com>
2018-03-08 15:25:04 -0800

    adding in action for ajax

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -34,15 +34,4 @@ $( document ).ready(function() {
       });
     })
   })
-
-	$(".contact-form").submit(function(e) {
-		e.preventDefault();
-
-		var $form = $(this);
-		$.post($form.attr("action"), $form.serialize()).then(function() {
-			$('form').fadeOut(function(){
-				$('.success-message').fadeIn();
-			});
-		});
-	});
 });
