b2e3acd7236cee8fe1b94eaa11935f50c4c17459
rickyahn0612 <rickyahn0612@yahoo.com>
2018-03-08 15:34:24 -0800

    adding ajax back in

--- source/javascripts/form_request.js
+++ source/javascripts/form_request.js
@@ -1,13 +1,13 @@
-//$(document).ready(function() {
-	//$(".contact-form").submit(function(e) {
-		//e.preventDefault();
+$(document).ready(function() {
+  $(".contact-form").submit(function(e) {
+    e.preventDefault();
 
-		//var $form = $(this);
-		//$.post($form.attr("action"), $form.serialize()).then(function() {
-			//$('form').fadeOut(function(){
-				//$('.success-message').fadeIn();
-			//});
-		//});
-	//});
-//});
+    var $form = $(this);
+    $.post($form.attr("action"), $form.serialize()).then(function() {
+      $('form').fadeOut(function(){
+        $('.success-message').fadeIn();
+      });
+    });
+  });
+});
 
