b1d1d7594e5c24de79fba79167bb92fe2f5ed54a
rickyahn0612 <rickyahn0612@yahoo.com>
2018-03-08 16:38:26 -0800

    custom success

--- source/javascripts/form_request.js
+++ source/javascripts/form_request.js
@@ -1,13 +1,13 @@
-$(document).ready(function() {
-  $(".contact-form").submit(function(e) {
-    e.preventDefault();
+//$(document).ready(function() {
+  //$(".contact-form").submit(function(e) {
+    //e.preventDefault();
 
-    var $form = $(this);
-    console.log($form.serialize())
-    $.post($form.attr("action"), $form.serialize()).then(function() {
-      $('form').fadeOut(function(){
-      });
-    });
-  });
-});
+    //var $form = $(this);
+    //$.post($form.attr("action"), $form.serialize()).then(function() {
+      //console.log($form.attr('action'))
+      //$('form').fadeOut(function(){
+      //});
+    //});
+  //});
+//});
 
