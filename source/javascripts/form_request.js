17cf6197efe2d4a450518b93e9daa7c3fb05c4d1
rickyahn0612 <rickyahn0612@yahoo.com>
2018-03-08 17:08:32 -0800

    texting ajax with recaptcha

--- source/javascripts/form_request.js
+++ source/javascripts/form_request.js
@@ -1,13 +1,13 @@
-//$(document).ready(function() {
-  //$(".contact-form").submit(function(e) {
-    //e.preventDefault();
+$(document).ready(function() {
+  $(".contact-form").submit(function(e) {
+    e.preventDefault();
 
-    //var $form = $(this);
-    //$.post($form.attr("action"), $form.serialize()).then(function() {
-      //console.log($form.attr('action'))
-      //$('form').fadeOut(function(){
-      //});
-    //});
-  //});
-//});
+    var $form = $(this);
+    $.post($form.attr("action"), $form.serialize()).then(function() {
+      console.log($form.attr('action'))
+      $('form').fadeOut(function(){
+      });
+    });
+  });
+});
 
