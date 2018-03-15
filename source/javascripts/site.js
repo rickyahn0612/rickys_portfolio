2899034b5f3d8e3949adf2a90bd1c0420c2f4c70
rickyahn0612 <rickyahn0612@yahoo.com>
2018-03-14 17:38:19 -0700

    new layout

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -9,30 +9,42 @@ $( document ).ready(function() {
     $('.left-container').fadeOut(function(){
 			scrollToTop();
     });
-    $('.right-container').addClass('mobile-view').animate({'left': 0}, 1000, function(){
+    $('.right-container').addClass('mobile-view').animate({'left': '-25%'}, 1000, function(){
       $('.port-view-container').fadeIn(700, function(){
         $('.close-button').animate({top: 0}, 800).addClass('close-button-mobile')
       });
     });
   });
 
-  $('.contact-form-link').on('click', function(e){
+  $('.contact-form-link, .mailer-icon').on('click', function(e){
     e.preventDefault();
-    $('.left-container').fadeOut(function() {
+    $('.left-container, .port-view-container').fadeOut(function() {
 			scrollToTop();
+      $('.mailer-icon').fadeOut();
       $('.close-button').animate({top: 0}, 800).addClass('close-button-mobile')
+      $('.right-container').removeClass('mobile-view').animate({'left': '45%'}, 1000)
       $('.contact-form-container').animate({top: 0}, 1500, function(){
         $(this).find('h1').addClass('glitch');
       });
+
+			var isVisible = $('.contact-form-container').is(":visible");
+			localStorage.setItem('visible', isVisible);
+
     });
+
   });
+
+	var isVisible = localStorage.getItem('visible') === 'false' ? false : true;
+	$('.link').toggle(isVisible);
+	console.log(isVisible)
 
   $('.close-button, .return-home').click(function(){
     $('.close-button').animate({top: -500}, 500).removeClass('close-button-mobile', function() {
       $('.port-view-container').fadeOut(function() {
-        $('.right-container').removeClass('mobile-view').animate({'left': '50%'}, 1000)
+        $('.right-container').removeClass('mobile-view').animate({'left': '45%'}, 1000)
         $('.left-container').fadeIn()
         $('.contact-form-container').animate({top: '-1000px'});
+        $('.mailer-icon').fadeIn();
 				scrollToTop();
       });
     })
@@ -45,4 +57,11 @@ $( document ).ready(function() {
   $('.portfolio-image-container').mouseleave(function(){
     $('.port-overlay').fadeOut();
   });
+
+	$('.mailer-icon').mouseenter(function(){
+    $(this).find('svg').removeClass('fa-envelope').addClass('fa-envelope-open')
+	})
+	$('.mailer-icon').mouseleave(function(){
+    $(this).find('svg').removeClass('fa-envelope-opne').addClass('fa-envelope')
+	})
 });
