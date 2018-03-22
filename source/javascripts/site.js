2111e7efe5e95085b3f576f51d06faa1f4ab4193
rickyahn0612 <rickyahn0612@yahoo.com>
2018-03-22 16:50:10 -0700

    implementing scroll state

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,24 +1,15 @@
 // This is where it all goes :)
 
-$( document ).ready(function() {
-    var learnMoreBtn = $('.learn-more-button')
-    var parentLeftContainer = $('.parent-left-container')
-    var halfParentContainer = '48vw'
-    var showRickyImage = $('.ricky-image').animate({'opacity' : 1}, 1000)
-    var introCopy = $('.intro-copy')
-    function showMainCopy() { $('.parent-left-container > .main-copy').fadeIn(); }
-    function mailerIcon() { $('.mailer-icon').fadeIn() }
+//$( document ).ready(function() {
+    //var learnMoreBtn = $('.learn-more-button')
+    //var parentLeftContainer = $('.parent-left-container')
+    //var halfParentContainer = '48vw'
+    //var showRickyImage = $('.ricky-image').animate({'opacity' : 1}, 1000)
+    //var introCopy = $('.intro-copy')
+    //function showMainCopy() { $('.parent-left-container > .main-copy').fadeIn(); }
+    //function mailerIcon() { $('.mailer-icon').fadeIn() }
 
-    $('.intro-copy > strong').animate({'opacity': '1', 'top': '0'}, 1500, function() {
-      $('.intro-copy > span').animate({'opacity' : '1'}, 500 , function() {
-        learnMoreBtn.animate({'opacity': '1', 'margin-top': '1.4vh'}, 700)
+    //$('.intro-copy > strong').animate({'opacity': '1', 'top': '0'}, 1500)
 
-        learnMoreBtn.click(function() {
-          introCopy.remove();
-          mailerIcon()
-          showRickyImage
-          showMainCopy()
-      });
-    });
-  });
-});
+  //});
+//});
---
+++ source/javascripts/smoothState.js
@@ -0,0 +1,45 @@
+(function($) {
+  $.fn.onPageLoad = function() {
+    $('.intro-copy > strong').animate({'opacity' : 1, 'top' : 0}, 500, function(){
+			$('.intro-copy > span').animate({'opacity' : 1}, 1500, function(){
+				$('.learn-more-button').animate({'opacity' : 1, 'margin-top' : '10px'}, 500)
+			})
+		})
+  };
+}(jQuery));
+
+$(document).ready(function() {
+  $('body').onPageLoad();
+});
+
+$(function() {
+  "use strict";
+  var $page = $("#main"),
+    options = {
+      debug: true,
+      prefetch: true,
+      cacheLength: 4,
+      onStart: {
+        duration: 800,
+        render: function($container) {
+          // Add your CSS animation reversing class
+          $container.addClass("is-exiting");
+          // Restart your animation
+					smoothState.restartCSSAnimations();
+        }
+      },
+      onReady: {
+        duration: 0,
+        render: function($container, $newContent) {
+          // Remove your CSS animation reversing class
+          $container.removeClass("is-exiting");
+          // Inject the new content
+          $container.html($newContent);
+        }
+      },
+			onAfter: function($container) {
+				$container.onPageLoad();
+			}
+    },
+    smoothState = $page.smoothState(options).data("smoothState");
+});
